5862cedfcd2fceb22f8fffa0e784d965d771e198
Фарид Наурзгалиев <faridnaurzgaliev@gmail.com>
2023-02-02 18:28:03 +0400

    2

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from rest_framework import serializers
 
 from reviews.models import Review, Comment, User
@@ -5,19 +6,34 @@ from reviews.models import Review, Comment, User
 
 class ReviewSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        default=serializers.CurrentUserDefault, queryset=User.objects.all(), slug_field='username'
+        default=serializers.CurrentUserDefault,
+        queryset=User.objects.all(),
+        slug_field='username'
+    )
+    score = serializers.IntegerField(
+        required=True,
+        validators=[MaxValueValidator(10), MinValueValidator(1)]
     )
 
     class Meta:
         model = Review
-        fields = '__all__'
+        exclude = ('title',)
+        read_only_fields = ('title', 'author')
+
+    def validate_title(self, value):
+        if value == self.context.get('request').pk:  # под вопросом, как это сделать?
+            raise serializers.ValidationError()
+        return value
 
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+        default=serializers.CurrentUserDefault,
+        queryset=User.objects.all(),
+        slug_field='username'
     )
 
     class Meta:
         model = Comment
-        fields = '__all__'
+        read_only_fields = ('author', 'review')
+        exclude = ('review',)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-
 User = get_user_model()
 
 
@@ -10,23 +9,11 @@ class Title(models.Model):
     name = models.CharField(max_length=60)
 
 
-class Rating(models.Model):
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='rating_author'
-    )
-    title = models.ForeignKey(
-        Title, on_delete=models.CASCADE, related_name='rating_title'
-    )
-
-    def __str__(self):
-        return f'{self.title}- {self.author}'
-
-    class Meta:
-        unique_together = ('author', 'title')
-
-
 class Review(models.Model):
     text = models.TextField('Отзыв')
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name='review'
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -35,7 +22,9 @@ class Review(models.Model):
     )
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     score = models.ForeignKey(
-        Rating, validators=[MinValueValidator(1), MaxValueValidator(10)], on_delete=models.CASCADE
+        'Рейтинг',
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+        on_delete=models.CASCADE
     )
 
     def __str__(self):
@@ -44,10 +33,19 @@ class Review(models.Model):
     class Meta:
         verbose_name = 'Отзыв'
         verbose_name_plural = 'Отзывы'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('title', 'author'),
+                name='unique_author_title'
+            )
+        ]
 
 
 class Comment(models.Model):
     text = models.TextField('Комментарий', max_length=5000)
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comment_review'
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -62,3 +60,17 @@ class Comment(models.Model):
     class Meta:
         verbose_name = 'Комментарий'
         verbose_name_plural = 'Комментарии'
+
+# class Rating(models.Model):
+#     author = models.ForeignKey(
+#         User, on_delete=models.CASCADE, related_name='rating_author'
+#     )
+#     title = models.ForeignKey(
+#         Title, on_delete=models.CASCADE, related_name='rating_title'
+#     )
+#
+#     def __str__(self):
+#         return f'{self.title}- {self.author}'
+#
+#     class Meta:
+#         unique_together = ('author', 'title')
