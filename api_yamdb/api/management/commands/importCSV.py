f4aaeec5e01bd7e58e5a8eab300fd4267ce6c189
Александр Мочалин <mochalin.av7@gmail.com>
2023-02-03 17:09:07 +0500

    fix User

--- api_yamdb/api/management/commands/importCSV.py
+++ api_yamdb/api/management/commands/importCSV.py
@@ -9,6 +9,19 @@ from reviews.models import (Category, Comment, Genre,
 
 class Command(BaseCommand):
     '''Комнда для импорта данных из .csv'''
+    def user_transfer(self):
+        with open(os.path.join(settings.BASE_DIR, 'static/data/users.csv'),
+                  'r', encoding='utf-8') as csv_f:
+            file_reader = csv.reader(csv_f, delimiter=",")
+
+            for r in file_reader:
+                if r[0] != 'id':
+                    User.objects.get_or_create(
+                        id=r[0], username=r[1], email=r[2],
+                        role=r[3], bio=r[4],
+                        first_name=[5], last_name=r[6]
+                    )
+
     def category_transfer(self):
         with open(os.path.join(settings.BASE_DIR, 'static/data/category.csv'),
                   'r', encoding='utf-8') as csv_f:
@@ -67,7 +80,7 @@ class Command(BaseCommand):
                 if r[0] != 'id':
                     Comment.objects.get_or_create(
                         id=r[0], review_id=r[1], text=r[2],
-                        author=r[3], pub_date=r[4]
+                        author_id=r[3], pub_date=r[4]
                     )
 
     def review_transfer(self):
@@ -81,28 +94,14 @@ class Command(BaseCommand):
                 if r[0] != 'id':
                     Review.objects.get_or_create(
                         id=r[0], title_id=r[1], text=r[2],
-                        author=r[3], score=r[4], pub_date=r[5]
-                    )
-
-    def user_transfer(self):
-        with open(os.path.join(settings.BASE_DIR, 'static/data/users.csv'),
-                  'r', encoding='utf-8') as csv_f:
-            file_reader = csv.reader(csv_f, delimiter=",")
-
-            for r in file_reader:
-                if r[0] != 'id':
-                    User.objects.get_or_create(
-                        id=r[0], username=r[1], email=r[2],
-                        role=r[3], bio=r[4],
-                        first_name=[5], last_name=r[6]
+                        author_id=r[3], score_id=r[4], pub_date=r[5]
                     )
 
     def handle(self, *args, **options):
+        self.user_transfer()
         self.category_transfer()
         self.genre_transfer()
         self.title_transfer()
         self.title_genre_transfer()
-        #  self.comment_transfer() нужен юзер
-        #  self.review_transfer() нужен юзер
-        #  self.user_transfer() не работает: django.db.utils.OperationalError:
-        #  no such table: reviews_user
+        #  self.comment_transfer()
+        #  self.review_transfer()
