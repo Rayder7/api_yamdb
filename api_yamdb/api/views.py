30e94be8dab94118b683f4dd654364b035031c41
Александр Мочалин <mochalin.av7@gmail.com>
2023-02-07 20:44:10 +0500

    1th review

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -11,7 +11,7 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from api_yamdb.settings import ADMIN_EMAIL
-from reviews.models import (Category, Comment, Genre, Review,
+from reviews.models import (Category, Genre, Review,
                             Title, User)
 from .serializers import (CategorySerializer, CommentSerializer,
                           GenreSerializer, ReviewSerializer,
